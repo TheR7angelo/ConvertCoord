90a8e2a5809d621763ec434c032dd8766e00511a
ZP6177 <raphael.antunes@hotmail.fr>
2022-07-07 09:44:00 +0200

    init

--- src/build/mods/__init__.py
+++ src/build/mods/__init__.py
@@ -1,5 +1,4 @@
 from src.build.mods.Functions import Functions
 from src.build.mods.Json import Json
 from src.build.mods.Maj import Maj
-from src.build.mods.Admin import has_admin
 from src.build.mods.worker import Worker_export, Worker_export_file, Worker_export_bdd, Worker_xlsx_bdd, Worker_csv_bdd
